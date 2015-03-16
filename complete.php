1fb1ee76e923f98d72455b1c678ac633a6be2b18
Ishan Madan <imadan3@uic.edu>
2015-03-16 11:34:32 -0500

    Adding complete.php
    php file for displaying of "successful completion of uploading images" message.

---
+++ complete.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+</head>
+  <body>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div style="width:650px; margin:0 auto; border-style:solid;border-color:#FF0000;">
+    <?php
+
+      echo "<h2> Images successfully uploaded in the central IBEIS database </h2>";
+      
+    ?>
+</div>
+
+</body>
+</html>
